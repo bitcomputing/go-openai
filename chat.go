287d19fb9e4c3cf0bbdf7d66b33f2b033e03831c
jogle <jiugea@gmail.com>
2025-06-24 10:47:42 +0800

    增加文件附件支持

--- chat.go
+++ chat.go
@@ -79,17 +79,25 @@ type ChatMessageImageURL struct {
 	Detail ImageURLDetail `json:"detail,omitempty"`
 }
 
+type ChatMessageFile struct {
+	FileData string `json:"file_data,omitempty"` // The base64 encoded file data, used when passing the file to the model as a string.
+	FileId   string `json:"file_id,omitempty"`   //The ID of an uploaded file to use as input.
+	FileName string `json:"file_name,omitempty"` //The name of the file, used when passing the file to the model as a string.
+}
+
 type ChatMessagePartType string
 
 const (
 	ChatMessagePartTypeText     ChatMessagePartType = "text"
 	ChatMessagePartTypeImageURL ChatMessagePartType = "image_url"
+	ChatMessagePartTypeFile     ChatMessagePartType = "file"
 )
 
 type ChatMessagePart struct {
 	Type     ChatMessagePartType  `json:"type,omitempty"`
 	Text     string               `json:"text,omitempty"`
 	ImageURL *ChatMessageImageURL `json:"image_url,omitempty"`
+	File     *ChatMessageFile     `json:"file,omitempty"`
 }
 
 type ChatCompletionMessage struct {
